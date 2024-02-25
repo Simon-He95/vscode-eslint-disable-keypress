68c665f521034287c344b0231da17c83435acbc1
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-02-25 18:22:03 +0800

    feat: 支持自动补充禁用的规则

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import { nextTick } from 'node:process'
-import { createCompletionItem, createPosition, getLineText, getOffsetFromPosition, getPosition, getSelection, jumpToLine, registerCommand, registerCompletionItemProvider, updateText } from '@vscode-use/utils'
+import { createCompletionItem, createPosition, nextTick, saveFile, getCurrentFileUrl, getLineText, getOffsetFromPosition, getPosition, getSelection, jumpToLine, message, registerCommand, registerCompletionItemProvider, updateText, getRootPath } from '@vscode-use/utils'
 import type { Disposable, ExtensionContext } from 'vscode'
 import { options } from './constants'
+import { jsShell } from 'lazy-js-utils'
+
 
 export async function activate(context: ExtensionContext) {
   const disposes: Disposable[] = []
@@ -14,11 +15,42 @@ export async function activate(context: ExtensionContext) {
       return true
     return false
   }
-  disposes.push(registerCommand('vscode-eslint-disable-keypress.on', () => {
+  let isWorking = false
+  disposes.push(registerCommand('vscode-eslint-disable-keypress.on', async () => {
+    if (isWorking)
+      return
+
     const selection = getSelection()
     if (!selection)
       return
+
     let insetText = ''
+    const errors = []
+    isWorking = true
+
+    const { result } = jsShell(`npx eslint ${getCurrentFileUrl()}`, {
+      cwd: getRootPath(),
+      isLog: true,
+      stdio: 'pipe'
+    })
+
+    if (result) {
+      const data = result.split('\n').map(i => i.trim().replace(/\s+/g, ' ')).slice(1)
+      for (const error of data) {
+        if (!error)
+          break
+        const [pos, e, ...rest] = error.split(' ')
+        const msg = rest.slice(0, -1).join(' ')
+        const rule = rest.slice(-1)[0]
+        const [line, character] = pos.split(':').map(i => +i)
+        errors.push({
+          line,
+          character,
+          msg,
+          rule
+        })
+      }
+    }
     if (selection.selectedTextArray.length > 1) {
       // /* eslint-disable */  /* eslint-enable */
       // todo: 多个选择快触发
@@ -26,9 +58,22 @@ export async function activate(context: ExtensionContext) {
     else if (isBlock(selection.selectedTextArray[0])) {
       const offset = getOffsetFromPosition(createPosition(selection.line, selection.character))!
       const newEndPosition = getPosition(offset + selection.selectedTextArray[0].length)
-      insetText = '/* eslint-disable  */\n'
+      const startPosition = createPosition(selection.line, 0)
+      if (!errors.length)
+        insetText = '/* eslint-disable  */\n'
+      else {
+        const temp = []
+        for (const item of errors) {
+          const { line, rule } = item
+          if (line >= startPosition.line + 1 && line <= newEndPosition.line + 1) {
+            temp.push(rule)
+          } else if (line > newEndPosition.line + 1)
+            break
+        }
+        insetText = `/* eslint-disable ${temp.join(',')} */\n`
+      }
       updateText((edit) => {
-        edit.insert(createPosition(selection.line, 0), insetText)
+        edit.insert(startPosition, insetText)
         edit.insert(createPosition(newEndPosition.line + 1, 0), '/* eslint-enable */\n')
       })
       nextTick(() => {
@@ -45,7 +90,19 @@ export async function activate(context: ExtensionContext) {
       }
       else {
         // 下一行eslint eslint-disable-next-line
-        insetText = '// eslint-disable-next-line\n'
+        if (!errors.length)
+          insetText = '// eslint-disable-next-line\n'
+        else {
+          const temp = []
+          for (const item of errors) {
+            const { line, rule } = item
+            if (line === selection.line + 1) {
+              temp.push(rule)
+            } else if (line > selection.line + 1)
+              break
+          }
+          insetText = `// eslint-disable-next-line ${temp.join(',')}\n`
+        }
 
         updateText((edit) => {
           edit.insert(createPosition(selection.line, 0), insetText)
@@ -55,6 +112,8 @@ export async function activate(context: ExtensionContext) {
         })
       }
     }
+    saveFile()
+    isWorking = false
   }))
 
   disposes.push(registerCompletionItemProvider('*', () => {
